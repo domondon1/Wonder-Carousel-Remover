3f63f4539db1f2895a1725cec2a6178dd26470fc
domondon1 <iea.domondon@gmail.com>
2017-07-18 18:50:36 +0800

    Add files via upload

---
+++ wonder-carousel-remover.php
@@ -0,0 +1,44 @@
+<?php 
+    /*
+    Plugin Name: Wonder-Carousel (Watermark Remover)
+    Plugin URI: https://github.com/domondon1/wonder-carousel-watermark-remove
+    Description: This Wordpress plugin is for the wonder carousel it will remove the watermark of the carousel
+    Author: Roderick C. Domondon
+    Version: 1
+    Author URI: https://github.com/domondon1
+    */
+
+    // Add 'hidden' class
+    function wonder_style() {
+        $style = "";
+        $style .= "<style>";
+        $style .= ".hidden{
+            visibility: hidden !important;
+            width:0 !important;
+            height: 0 !important;
+            padding: 0 !important;
+            overflow: hidden !important;
+        }";
+        $style .= "</style>";
+        echo $style;
+    }
+
+    // This script it will append after 3 seconds so that it will disregard the carousel code
+    function wonder_script() {
+        $url = 'a[href="http://www.wonderplugin.com/wordpress-carousel/"]';
+        $script = "";
+        $script .= "<script>";
+        $script .= "jQuery(document).ready(function(){
+            setTimeout(function() {
+                jQuery('.amazingcarousel-image').find('".$url."').parent().addClass('hidden').removeAttr( 'style' );
+            }, 3000);
+        });";
+        $script .= "</script>";
+        echo $script;
+    }
+
+
+    add_action('wp_head', 'wonder_style');
+
+    add_action('wp_head', 'wonder_script');
+    ?>
